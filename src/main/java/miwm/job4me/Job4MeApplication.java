b75d664fe778a73d1110b9cb309b88f81964234a
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-07 15:09:14 +0200

    Initial commit

---
+++ src/main/java/miwm/job4me/Job4MeApplication.java
@@ -0,0 +1,13 @@
+package miwm.job4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4MeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4MeApplication.class, args);
+	}
+
+}
