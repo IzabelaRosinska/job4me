d66bcb5e665706e0d777d2aa10c9d3ab28f61ac9
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-09 20:03:53 +0200

    MIWM-167: Create auth exception

--- src/main/java/miwm/job4me/messages/UserMessages.java
+++ src/main/java/miwm/job4me/messages/UserMessages.java
@@ -4,5 +4,8 @@ public class UserMessages {
 
     public final static String ACCOUNT_FOR_EMAIL_EXISTS = "There is an account with that email address: ";
     public final static String USER_EXISTS = "Takie konto juz istnieje!";
+    public final static String INVALID_TOKEN = "Token %s cannot be trusted";
+    public final static String INVALID_PASSWORD = "Invalid password";
+    public final static String BANNED_RESOURCES = "Nie masz dostepu do tych zasobow";
 
 }
