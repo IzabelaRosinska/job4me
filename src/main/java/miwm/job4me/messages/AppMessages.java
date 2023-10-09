b5095bd62a048eec8a1c449c4cc186f9e040bc12
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-09 20:28:41 +0200

    MIWM-160: Configure HTTP session

---
+++ src/main/java/miwm/job4me/messages/AppMessages.java
@@ -0,0 +1,6 @@
+package miwm.job4me.messages;
+
+public class AppMessages {
+
+    public static final String JWT_TOKEN_NAME = "token";
+}
---
+++ src/main/java/miwm/job4me/security/ApplicationSecurityConfig.java
@@ -0,0 +1,63 @@
+package miwm.job4me.security;
+
+import miwm.job4me.jwt.JwtConfig;
+import miwm.job4me.messages.AppMessages;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.http.Cookie;
+
+
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+    public ApplicationSecurityConfig(SecretKey secretKey, JwtConfig jwtConfig) {
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().csrf().disable()
+
+                    .authorizeRequests()
+                    .antMatchers("/employee/**").hasRole("EMPLOYEE")
+                    .antMatchers("/employer/**").hasRole("EMPLOYER")
+                    .antMatchers("/fragments/**").hasAnyRole("EMPLOYEE","EMPLOYER")
+                    .antMatchers("/home").permitAll()
+                    .antMatchers("/signup").permitAll()
+
+                    .and().formLogin().loginPage("/login").permitAll().successHandler(appAuthenticationSuccessHandler())
+                    .and().httpBasic()
+                    .and().logout(logout -> logout
+                            .logoutUrl("/logout")
+                            .addLogoutHandler((request, response, auth) -> {
+                                for (Cookie cookie : request.getCookies()) {
+                                    String cookieName = cookie.getName();
+                                    if(cookieName.equals(AppMessages.JWT_TOKEN_NAME)) {
+                                        Cookie cookieToDelete = new Cookie(cookieName, null);
+                                        cookieToDelete.setMaxAge(0);
+                                        response.addCookie(cookieToDelete);
+                                    }
+                                }
+                            })
+                          );
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler appAuthenticationSuccessHandler() {
+        return new ApplicationAuthenticationSuccessHandler();
+    }
+}
