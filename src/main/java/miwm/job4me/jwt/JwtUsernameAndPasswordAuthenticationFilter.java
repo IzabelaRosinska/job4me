cc42a2b430f9d98fa9c3aea9402fa0824ec2011d
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-09 21:30:45 +0200

    MIWM-164: Implement user authentication

---
+++ src/main/java/miwm/job4me/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package miwm.job4me.jwt;
+
+import io.jsonwebtoken.Jwts;
+import miwm.job4me.messages.AppMessages;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+
+import static miwm.job4me.security.ApplicationUserRole.EMPLOYEE;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager, JwtConfig jwtConfig, SecretKey secretKey) {
+        this.authenticationManager = authenticationManager;
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, password);
+
+        setDetails(request, authentication);
+        Authentication authenticate = authenticationManager.authenticate(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return authenticate;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
+                .signWith(secretKey)
+                .compact();
+        response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + token);
+
+        Cookie tokenCookie = new Cookie(AppMessages.JWT_TOKEN_NAME, token);
+        tokenCookie.setHttpOnly(true);
+        response.addCookie(tokenCookie);
+
+        if(authResult.getAuthorities().contains(EMPLOYEE.getUserRole()))
+            response.sendRedirect("/employee/account");
+        else
+            response.sendRedirect("/employer/account");
+    }
+}
+
