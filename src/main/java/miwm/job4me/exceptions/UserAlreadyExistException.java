97c981f3fab324f99ecabac325c07f0362747d23
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-10 19:26:45 +0200

    MIWM-159: Create user already exists exception (#3)

---
+++ src/main/java/miwm/job4me/exceptions/UserAlreadyExistException.java
@@ -0,0 +1,16 @@
+package miwm.job4me.exceptions;
+
+public class UserAlreadyExistException extends RuntimeException {
+
+    public UserAlreadyExistException() {
+        super();
+    }
+
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/miwm/job4me/messages/UserMessages.java
@@ -0,0 +1,8 @@
+package miwm.job4me.messages;
+
+public class UserMessages {
+
+    public final static String ACCOUNT_FOR_EMAIL_EXISTS = "There is an account with that email address: ";
+    public final static String USER_EXISTS = "Takie konto juz istnieje!";
+
+}
