016203ec45d8ec588492211c9dceb4ee8c75cde8
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-14 18:19:37 +0200

    MIWM-163: Implement user token verifier (#8)

---
+++ src/main/java/miwm/job4me/jwt/JwtTokenVerifier.java
@@ -0,0 +1,71 @@
+package miwm.job4me.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import miwm.job4me.messages.AppMessages;
+import miwm.job4me.messages.UserMessages;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+
+    private final SecretKey secretKey;
+
+    public JwtTokenVerifier(SecretKey secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        String cookieToken = "";
+        for(Cookie cookie : cookies )
+            if((AppMessages.JWT_TOKEN_NAME).equals(cookie.getName()))
+                cookieToken = cookie.getValue();
+
+        if(!cookieToken.equals("")) {
+            try {
+                Jws<Claims> claimsJws = Jwts.parser()
+                        .setSigningKey(secretKey)
+                        .parseClaimsJws(cookieToken);
+
+                Claims body = claimsJws.getBody();
+                String username = body.getSubject();
+                var authorities = (List<Map<String, String>>) body.get("authorities");
+
+                Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                        .map(m -> new SimpleGrantedAuthority(m.get("authority")))
+                        .collect(Collectors.toSet());
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, simpleGrantedAuthorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (JwtException exception) {
+                throw new IllegalStateException(String.format(UserMessages.INVALID_TOKEN, cookieToken));
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().equals("/login");
+    }
+}
