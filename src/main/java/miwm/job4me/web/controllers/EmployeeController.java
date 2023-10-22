ad093424fa51c3d1fe94f95537ba1385920fcff8
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-10-22 22:04:52 +0200

    MIWM-208: Add Swagger (#29)

--- src/main/java/miwm/job4me/web/controllers/EmployeeController.java
+++ src/main/java/miwm/job4me/web/controllers/EmployeeController.java
@@ -1,5 +1,6 @@
 package miwm.job4me.web.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import miwm.job4me.services.users.EmployeeService;
 import miwm.job4me.web.model.users.EmployeeDto;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +17,13 @@ public class EmployeeController {
     }
 
     @GetMapping("/employee/cv")
+    @Operation(summary = "Gets current employee's CV", description = "Gets current employee's data restricted to CV scope")
     public EmployeeDto getCV() {
         return employeeService.findCurrentEmployee();
     }
 
     @PutMapping("/employee/cv")
+    @Operation(summary = "Updates current employee's CV", description = "Updates current employee's data restricted to CV scope")
     public EmployeeDto updateCV(@RequestBody EmployeeDto employeeDto) {
         return employeeService.updateCV(employeeDto);
     }
--- src/main/java/miwm/job4me/web/model/users/EmployeeDto.java
+++ src/main/java/miwm/job4me/web/model/users/EmployeeDto.java
@@ -1,5 +1,6 @@
 package miwm.job4me.web.model.users;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,15 +9,36 @@ import java.util.ArrayList;
 @Getter
 @Setter
 public class EmployeeDto {
+    @Schema(description = "Id of the employee", example = "1")
     private Long id;
+
+    @Schema(description = "First name of the employee", example = "John")
     private String firstName;
+
+    @Schema(description = "Last name of the employee", example = "Smith")
     private String lastName;
+
+    @Schema(description = "Contact email of the employee", example = "john.smith@gmail.com")
     private String email;
+
+    @Schema(description = "Contact telephone of the employee", example = "123456789")
     private String telephone;
+
+    @Schema(description = "About me section of the employee", example = "I am passionate about Java.")
     private String aboutMe;
+
+    @Schema(description = "Interests section of the employee", example = "I collect post-stamps.")
     private String interests;
+
+    @Schema(description = "List of Education information of the employee", example = "[\"2020-present: Wroclaw University of Science and Technology, applied computer science\"]")
     private ArrayList<String> education;
+
+    @Schema(description = "List of Experience information of the employee", example = "[\"1 year - Junior Java web developer\"]")
     private ArrayList<String> experience;
+
+    @Schema(description = "List of Projects information of the employee", example = "[\"Library Web App - project created in Java\"]")
     private ArrayList<String> projects;
+
+    @Schema(description = "List of Skills information of the employee", example = "[\"Java\", \"Spring\", \"SQL\"]")
     private ArrayList<String> skills;
 }
