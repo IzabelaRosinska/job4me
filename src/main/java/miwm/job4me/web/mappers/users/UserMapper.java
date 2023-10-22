b124b7346256ad0def3849df9e56ac0751dda9cd
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-10-22 18:51:00 +0200

    MIWM-158: Fix getters

--- src/main/java/miwm/job4me/web/mappers/users/UserMapper.java
+++ src/main/java/miwm/job4me/web/mappers/users/UserMapper.java
@@ -7,7 +7,7 @@ public class UserMapper {
 
     public UserDto userToUserDto(Person user) {
         UserDto userDto = UserDto.builder()
-                .email(user.getEmail())
+                .username(user.getEmail())
                 .password(user.getPassword())
                 .role(user.getUserRole().getAuthority()).build();
         return userDto;
