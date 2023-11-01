77975a550f951e4fbf4835ab2dc81c98baa93160
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-01 11:27:17 +0100

    MIWM-236: Fix email secrets

--- src/main/java/miwm/job4me/web/controllers/AuthController.java
+++ src/main/java/miwm/job4me/web/controllers/AuthController.java
@@ -15,8 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.view.RedirectView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
