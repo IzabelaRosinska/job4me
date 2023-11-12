a4e547ea6c64c8346984e2353fa197896207b67b
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-12 09:59:39 +0100

    MIWM-238: Update contact email

--- src/main/java/miwm/job4me/web/mappers/users/EmployerMapper.java
+++ src/main/java/miwm/job4me/web/mappers/users/EmployerMapper.java
@@ -11,13 +11,23 @@ public class EmployerMapper {
         EmployerDto employerDto = new EmployerDto();
         employerDto.setId(employer.getId().toString());
         employerDto.setCompanyName(employer.getCompanyName());
-        employerDto.setContactEmail(employer.getContactEmail());
+        employerDto.setEmail(employer.getContactEmail());
         employerDto.setDescription(employer.getDescription());
         employerDto.setDisplayDescription(employer.getDisplayDescription());
         employerDto.setTelephone(employer.getTelephone());
-        employerDto.setEmail(employer.getEmail());
         employerDto.setPhoto(employer.getPhoto());
         employerDto.setAddress(employer.getAddress());
         return employerDto;
+    }
+
+    public Employer employerDtoToEmployer(EmployerDto employerDto, Employer employer) {
+        employer.setCompanyName(employerDto.getCompanyName());
+        employer.setDescription(employerDto.getDescription());
+        employer.setDisplayDescription(employerDto.getDisplayDescription());
+        employer.setTelephone(employerDto.getTelephone());
+        employer.setContactEmail(employerDto.getEmail());
+        employer.setPhoto(employerDto.getPhoto());
+        employer.setAddress(employerDto.getAddress());
+        return employer;
     }
 }
