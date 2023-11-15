ba89fb5514cac287a74908ed181afddb9ac38617
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-15 18:40:07 +0100

    MIWM-363: Fix JobFairRepository problem (#65)

--- src/main/java/miwm/job4me/repositories/event/JobFairRepository.java
+++ src/main/java/miwm/job4me/repositories/event/JobFairRepository.java
@@ -4,9 +4,11 @@ import miwm.job4me.model.event.JobFair;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobFairRepository extends JpaRepository<JobFair, Long> {
+    @Query("SELECT jf FROM JobFair jf")
     Page<JobFair> findAllByFilters(Pageable pageable);
 }
