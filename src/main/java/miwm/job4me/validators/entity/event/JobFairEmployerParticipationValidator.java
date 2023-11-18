d3fc550cac0abf5de7fc1cb15c86b9f4d19c12f8
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-18 19:10:49 +0100

    MIWM-371: Add validation do JobFairEmployerParticipationValidator

--- src/main/java/miwm/job4me/validators/entity/event/JobFairEmployerParticipationValidator.java
+++ src/main/java/miwm/job4me/validators/entity/event/JobFairEmployerParticipationValidator.java
@@ -1,14 +1,43 @@
 package miwm.job4me.validators.entity.event;
 
+import miwm.job4me.exceptions.InvalidArgumentException;
+import miwm.job4me.messages.ExceptionMessages;
+import miwm.job4me.model.event.JobFairEmployerParticipation;
+import miwm.job4me.web.model.event.JobFairEmployerParticipationDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobFairEmployerParticipationValidator {
-    public void validate() {
+    private String ENTITY_NAME = "JobFairEmployerParticipation";
+    private String JOB_FAIR_ENTITY_NAME = "JobFair";
+    private String EMPLOYER_ENTITY_NAME = "Employer";
+
+    public void validate(JobFairEmployerParticipation jobFairEmployerParticipation) {
+        if (jobFairEmployerParticipation == null) {
+            throw new InvalidArgumentException(ExceptionMessages.nullArgument(ENTITY_NAME));
+        }
+
+        if (jobFairEmployerParticipation.getJobFair() == null) {
+            throw new InvalidArgumentException(ExceptionMessages.notNull(ENTITY_NAME, JOB_FAIR_ENTITY_NAME));
+        }
+
+        if (jobFairEmployerParticipation.getEmployer() == null) {
+            throw new InvalidArgumentException(ExceptionMessages.notNull(ENTITY_NAME, EMPLOYER_ENTITY_NAME));
+        }
 
     }
 
-    public void validateDto() {
+    public void validateDto(JobFairEmployerParticipationDto jobFairEmployerParticipation) {
+        if (jobFairEmployerParticipation == null) {
+            throw new InvalidArgumentException(ExceptionMessages.nullArgument(ENTITY_NAME));
+        }
 
+        if (jobFairEmployerParticipation.getJobFairId() == null) {
+            throw new InvalidArgumentException(ExceptionMessages.notNull(ENTITY_NAME, JOB_FAIR_ENTITY_NAME));
+        }
+
+        if (jobFairEmployerParticipation.getEmployerId() == null) {
+            throw new InvalidArgumentException(ExceptionMessages.notNull(ENTITY_NAME, EMPLOYER_ENTITY_NAME));
+        }
     }
 }
