0bd592d01f7e249710326cb92b9a8be35ee43ddb
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-21 02:43:14 +0100

    MIWM-413: Resign from native query due to pagination

--- src/main/java/miwm/job4me/repositories/offer/JobOfferRepository.java
+++ src/main/java/miwm/job4me/repositories/offer/JobOfferRepository.java
@@ -12,32 +12,48 @@ import java.util.List;
 
 @Repository
 public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
-    @Query("SELECT DISTINCT j FROM JobOffer j " +
-            "LEFT JOIN j.localizations loc " +
-            "LEFT JOIN j.employmentForms empForm " +
-            "LEFT JOIN j.levels level " +
-            "LEFT JOIN j.contractTypes contractType " +
-            "LEFT JOIN j.industries industry " +
-            "WHERE :employerId IS NULL OR j.employer.id IN (:employerId) " +
-            "AND (:isActive IS NULL OR j.isActive = :isActive) " +
-            "AND (:cities IS NULL OR loc.city IN (:cities)) " +
-            "AND (:employmentFormNames IS NULL OR empForm.name IN (:employmentFormNames)) " +
-            "AND (:levelNames IS NULL OR level.name IN (:levelNames)) " +
-            "AND (:contractTypeNames IS NULL OR contractType.name IN (:contractTypeNames)) " +
-            "AND (:salaryFrom IS NULL OR j.salaryFrom >= :salaryFrom) " +
-            "AND (:salaryTo IS NULL OR j.salaryTo <= :salaryTo) " +
-            "AND (:industryNames IS NULL OR industry.name IN (:industryNames)) " +
-            "AND (:offerName IS NULL OR LOWER(j.offerName) LIKE LOWER(:offerName))")
+//    @Query("SELECT DISTINCT j FROM JobOffer j " +
+//            "LEFT JOIN j.localizations loc " +
+//            "LEFT JOIN j.employmentForms empForm " +
+//            "LEFT JOIN j.levels level " +
+//            "LEFT JOIN j.contractTypes contractType " +
+//            "LEFT JOIN j.industries industry " +
+//            "WHERE :employerId IS NULL OR j.employer.id IN (:employerId) " +
+//            "AND (:isActive IS NULL OR j.isActive = :isActive) " +
+//            "AND (:cities IS NULL OR loc.city IN (:cities)) " +
+//            "AND (:employmentFormNames IS NULL OR empForm.name IN (:employmentFormNames)) " +
+//            "AND (:levelNames IS NULL OR level.name IN (:levelNames)) " +
+//            "AND (:contractTypeNames IS NULL OR contractType.name IN (:contractTypeNames)) " +
+//            "AND (:salaryFrom IS NULL OR j.salaryFrom >= :salaryFrom) " +
+//            "AND (:salaryTo IS NULL OR j.salaryTo <= :salaryTo) " +
+//            "AND (:industryNames IS NULL OR industry.name IN (:industryNames)) " +
+//            "AND (:offerName IS NULL OR LOWER(j.offerName) LIKE LOWER(:offerName))")
+//    Page<JobOffer> findAllOffersByFilters(Pageable pageable,
+//                                          @Param("employerIds") List<Long> employerIds,
+//                                          @Param("isActive") Boolean isActive,
+//                                          @Param("cities") List<String> cities,
+//                                          @Param("employmentFormNames") List<String> employmentFormNames,
+//                                          @Param("levelNames") List<String> levelNames,
+//                                          @Param("contractTypeNames") List<String> contractTypeNames,
+//                                          @Param("salaryFrom") Integer salaryFrom,
+//                                          @Param("salaryTo") Integer salaryTo,
+//                                          @Param("industryNames") List<String> industryNames,
+//                                          @Param("offerName") String offerName);
+
+    @Query(value = "SELECT DISTINCT j.* FROM " +
+            "(SELECT DISTINCT j.* FROM job_offers j " +
+            "WHERE (?1 IS NULL OR j.employer_id IN ?1) " +
+            "AND (?2 IS NULL OR j.is_active = ?2) " +
+            "AND (?3 IS NULL OR j.salary_from >= ?3) " +
+            "AND (:salaryTo IS NULL OR j.salary_to <= :salaryTo) " +
+            "AND (:offerName IS NULL OR LOWER(j.offer_name) LIKE LOWER(CONCAT('%', :offerName, '%')))) j " +
+            "LEFT JOIN (SELECT loc.* FROM job_offer_localizations loc " +
+            "LEFT JOIN localizations l ON loc.localization_id = l.id" +
+            "WHERE (?5 IS NULL OR l.city IN ?5)) loc ON j.id = loc.job_offer_id ", nativeQuery = true)
     Page<JobOffer> findAllOffersByFilters(Pageable pageable,
-                                          @Param("employerIds") List<Long> employerIds,
-                                          @Param("isActive") Boolean isActive,
-                                          @Param("cities") List<String> cities,
-                                          @Param("employmentFormNames") List<String> employmentFormNames,
-                                          @Param("levelNames") List<String> levelNames,
-                                          @Param("contractTypeNames") List<String> contractTypeNames,
-                                          @Param("salaryFrom") Integer salaryFrom,
+                                          List<Long> employerIds,
+                                          Boolean isActive,
+                                          Integer salaryFrom,
                                           @Param("salaryTo") Integer salaryTo,
-                                          @Param("industryNames") List<String> industryNames,
-                                          @Param("offerName") String offerName,
-                                          @Param("order") String order);
+                                          @Param("offerName") String offerName);
 }
