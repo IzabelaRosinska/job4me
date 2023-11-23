5481116a8ab06bce52c18fd22e04604d8b5e921a
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-24 00:53:13 +0100

    MIWM-398: Add rest template

--- src/main/java/miwm/job4me/SimpleCORSFilter.java
+++ src/main/java/miwm/job4me/SimpleCORSFilter.java
@@ -24,11 +24,11 @@ public class SimpleCORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Accept, X-Requested-With, remember-me");
 
         chain.doFilter(req, res);
 
