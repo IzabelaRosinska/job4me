3a8fa6335e8e220c0f799344005964f45cef28f6
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-24 14:06:05 +0100

    MIWM-377: Adjust pipeline

--- src/main/java/miwm/job4me/web/controllers/recommendation/RecommendationController.java
+++ src/main/java/miwm/job4me/web/controllers/recommendation/RecommendationController.java
@@ -31,7 +31,7 @@ public class RecommendationController {
         return new ResponseEntity<>(listDisplaySavedDtoPage, HttpStatus.OK);
     }
 
-    @PostMapping("employee/job-offers/list-display/job-fair/{jobFairId}/filter/recommendation")
+    @PostMapping("employee/job-offers/list-display/job-fair/{jobFairId}/recommendation/filter")
     @Operation(summary = "Get all job offers of job fair by filter (get with body) for logged in employee with recommendation", description = "Gets all job offers of job fair from database by filter and returns them in list display saved dto with saved field set to true if job offer is saved by logged in employee with recommendation")
     public ResponseEntity<Page<ListDisplaySavedDto>> getAllOffersOfJobFairByFilterEmployeeView(@RequestParam(defaultValue = "0") int page,
                                                                                                @RequestParam(defaultValue = "10") int size,
