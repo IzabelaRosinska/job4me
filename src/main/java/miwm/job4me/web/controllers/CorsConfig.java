87089cb2a254e5ff9b72a7bb25c111e1b11b0f5e
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-25 14:40:08 +0100

    MIWM-398: Change config

--- src/main/java/miwm/job4me/web/controllers/CorsConfig.java
+++ src/main/java/miwm/job4me/web/controllers/CorsConfig.java
@@ -6,15 +6,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://mango-moss-0c13e2b03-32.westeurope.3.azurestaticapps.net", "http://localhost:4200", "https://www.linkedin.com")
-                .allowedMethods("POST, GET, PATCH, OPTIONS, DELETE, PUT")
-                .allowedHeaders("Origin, Content-Type, Authorization, Accept, X-Requested-With, remember-me")
+                .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600); // You can set the max age of the preflight request in seconds
     }
