491f5460c8b65734b14df833f5a850471c238866
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-25 13:51:09 +0100

    MIWM-398: Overwrite azure

---
+++ src/main/java/miwm/job4me/web/controllers/CorsConfig.java
@@ -0,0 +1,22 @@
+package miwm.job4me.web.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://mango-moss-0c13e2b03-32.westeurope.3.azurestaticapps.net", "http://localhost:4200", "https://www.linkedin.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Headers")
+                .allowCredentials(true)
+                .maxAge(3600); // You can set the max age of the preflight request in seconds
+    }
+}
