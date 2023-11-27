e31c913950adce5b0264ca368cbd705024aeda3d
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-27 22:18:50 +0100

    MIWM-452: Refactor controllers

--- src/main/java/miwm/job4me/web/controllers/AuthController.java
+++ src/main/java/miwm/job4me/web/controllers/AuthController.java
@@ -16,7 +16,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
