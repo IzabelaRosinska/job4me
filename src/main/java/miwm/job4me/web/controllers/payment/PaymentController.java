38848df4289c2cb3bccae6845f1e8e94ef5bc486
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-27 16:14:54 +0100

    MIWM-286: Add PaymentController

---
+++ src/main/java/miwm/job4me/web/controllers/payment/PaymentController.java
@@ -0,0 +1,35 @@
+package miwm.job4me.web.controllers.payment;
+
+import miwm.job4me.services.payment.PaymentService;
+import miwm.job4me.web.model.payment.PaymentCheckout;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping("/payment/organizer-account/success")
+    public String success() {
+        return "success";
+    }
+
+    @GetMapping("/payment/organizer-account/cancel")
+    public String cancel() {
+        return "cancel";
+    }
+
+    @GetMapping("/payment")
+    public ResponseEntity<PaymentCheckout> payForOrganizerAccount() {
+        PaymentCheckout paymentCheckout = new PaymentCheckout();
+        paymentCheckout.setUrl(paymentService.payForOrganizerAccount());
+        System.out.println(paymentCheckout.getUrl());
+
+        return ResponseEntity.ok(paymentCheckout);
+    }
+}
