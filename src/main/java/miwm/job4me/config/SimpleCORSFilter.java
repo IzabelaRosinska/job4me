5ae22ca102e0c311b1b3246c51cf5c962d426249
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-27 18:40:58 +0100

    MIWM-398: Change list

--- src/main/java/miwm/job4me/config/SimpleCORSFilter.java
+++ src/main/java/miwm/job4me/config/SimpleCORSFilter.java
@@ -1,6 +1,8 @@
 package miwm.job4me.config;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,12 +18,8 @@ public class SimpleCORSFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        //List<String> headers = new ArrayList<>(FRONT_BRANCH_HOST_AZURE, FRONT_BRANCH_HOST_AZURE, FRONT_HOST, LINKEDIN_HOST, "https://mango-moss-0c13e2b03-40.westeurope.3.azurestaticapps.net");
         response.setHeader("Access-Control-Allow-Origin", FRONT_BRANCH_HOST_AZURE);
-        response.addHeader("Access-Control-Allow-Origin", FRONT_HOST_AZURE);
-        response.addHeader("Access-Control-Allow-Origin", FRONT_HOST);
-        response.addHeader("Access-Control-Allow-Origin", LINKEDIN_HOST);
-        response.addHeader("Access-Control-Allow-Origin", "https://mango-moss-0c13e2b03-40.westeurope.3.azurestaticapps.net");
-
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
