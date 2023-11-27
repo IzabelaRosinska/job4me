239a3b4b4b6b13faad2ffea0fa78ee92e7063f81
MagdalenaBrejna <66904323+MagdalenaBrejna@users.noreply.github.com>
2023-11-27 18:29:19 +0100

    MIWM-398: Add urls

--- src/main/java/miwm/job4me/config/SimpleCORSFilter.java
+++ src/main/java/miwm/job4me/config/SimpleCORSFilter.java
@@ -8,12 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import static miwm.job4me.messages.AppMessages.*;
+
 @Service
 public class SimpleCORSFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        response.setHeader("Access-Control-Allow-Origin", FRONT_BRANCH_HOST_AZURE);
+        response.addHeader("Access-Control-Allow-Origin", FRONT_HOST_AZURE);
+        response.addHeader("Access-Control-Allow-Origin", FRONT_HOST);
+        response.addHeader("Access-Control-Allow-Origin", LINKEDIN_HOST);
+        response.addHeader("Access-Control-Allow-Origin", "https://mango-moss-0c13e2b03-40.westeurope.3.azurestaticapps.net");
+
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
