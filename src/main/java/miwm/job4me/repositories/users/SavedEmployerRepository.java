282f0a7a3c10f8a3f4b5c65d942c62eb9dfa6cb7
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-11-28 17:34:33 +0100

    MIWM-455: Add paging methods to repositories

--- src/main/java/miwm/job4me/repositories/users/SavedEmployerRepository.java
+++ src/main/java/miwm/job4me/repositories/users/SavedEmployerRepository.java
@@ -1,8 +1,11 @@
 package miwm.job4me.repositories.users;
 
 import miwm.job4me.model.users.SavedEmployer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,7 @@ public interface SavedEmployerRepository extends JpaRepository<SavedEmployer, Lo
 
     @Query(nativeQuery = true, value = "select * from saved_employers where employee_id = ?1")
     List<SavedEmployer> getSavedForEmployee(Long employeeId);
+
+    Page<SavedEmployer> findAllByEmployeeIdOrderByIdDesc(Pageable pageable, Long employeeId);
+
 }
